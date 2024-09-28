a31ea0825f26f9039659d9a6f24b96edbdf37465
destiny <botnari_ciprian@mail.ru>
2024-09-28 21:53:44 +0300

    feat: add concurrent tasks limit

--- gateway/index.js
+++ gateway/index.js
@@ -1,8 +1,29 @@
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
+const rateLimit = require('express-rate-limit');
 
 const app = express();
 const PORT = process.env.PORT || 3333;
+
+// Users service rate limiting configuration
+const usersLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 10,                  // Limit each IP to 10 requests every 15 minutes
+  message: {                // Message to send when the limit is exceeded
+    status: 429,
+    message: 'Too many requests to users service. Please try again later.'
+  }
+});
+
+// Game service rate limiting configuration
+const gamesLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 20,                  // Limit each IP to 20 requests every 15 minutes
+  message: {                // Message to send when the limit is exceeded
+    status: 429,
+    message: 'Too many requests to games service. Please try again later.'
+  }
+});
 
 // Proxy configuration
 const usersServiceProxy = createProxyMiddleware({
@@ -15,15 +36,15 @@ const gameServiceProxy = createProxyMiddleware({
   changeOrigin: true,
 });
 
-// Routes for users service
-app.use('/users/register', usersServiceProxy);
-app.use('/users/login', usersServiceProxy);
+// Routes for users service with rate limiter
+app.use('/users/register', usersLimiter, usersServiceProxy);
+app.use('/users/login', usersLimiter, usersServiceProxy);
 
-// Routes for game service
-app.use('/lobby', gameServiceProxy);
-app.use('/lobby/create', gameServiceProxy);
-app.use('/lobby/:id/join', gameServiceProxy);
-app.use('/lobby/:id/leave', gameServiceProxy);
+// Routes for game service with rate limiter
+app.use('/lobby', gamesLimiter, gameServiceProxy);
+app.use('/lobby/create', gamesLimiter, gameServiceProxy);
+app.use('/lobby/:id/join', gamesLimiter, gameServiceProxy);
+app.use('/lobby/:id/leave', gamesLimiter, gameServiceProxy);
 
 // Default route
 app.get('/', (req, res) => {
@@ -35,6 +56,14 @@ app.use((req, res, next) => {
   res.status(401).json({ 
     status: 401,
     message: 'Accessing an unauthorized resource' 
+  });
+});
+
+// Handle 404 errors
+app.use((req, res, next) => {
+  res.status(404).json({ 
+    status: 404,
+    message: 'Resource not found' 
   });
 });
 
